8eb593d4b6600f3984826b323ce7899d726919f2
muhammad.abtahi <muhammad.abtahi@i-splendid.com>
2024-12-18 15:02:46 +0500

    Map widget name changes.

--- lib/utils/SAMapWidget.dart
+++ lib/utils/SAMapWidget.dart
@@ -3,8 +3,8 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class GoogleMapsPage extends StatelessWidget {
-  GoogleMapsPage({
+class SAMapWidget extends StatelessWidget {
+  SAMapWidget({
     super.key,
     this.onCameraMove,
     this.onCameraIdle,
